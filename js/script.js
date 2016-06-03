db033d4312becf618a394b1e80b8f5fb7cd7e9cc
Agronin Aleksey <agronin.al@gmail.com>
2016-06-03 09:04:44 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,94 @@
+$('document').ready(function(){
+    $('.carousel').carousel();
+    $("#owl").owlCarousel({
+
+        items:3,
+        loop:true,
+        margin:20,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            800:{
+                items:2
+            },
+            1100:{
+                items:3
+            }
+        }
+    });
+    $('.play').on('click',function(){
+        owl.trigger('autoplay.play.owl',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('autoplay.stop.owl')
+ 
+})
+    $("#products").owlCarousel({
+        items:4,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            800:{
+                items:2
+            },
+            1100:{
+                items:4
+            }
+        }
+    });
+    $('.play').on('click',function(){
+        owl.trigger('autoplay.play.owl',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('autoplay.stop.owl')
+    });
+
+    $("#blog-items").owlCarousel({
+
+        items:3,
+        loop:true,
+        margin:20,
+        autoplay:true,
+        autoplayTimeout:6000,
+        autoplayHoverPause:true,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            800:{
+                items:2
+            },
+            1100:{
+                items:3
+            }
+        }
+    });
+    $('.play').on('click',function(){
+        owl.trigger('autoplay.play.owl',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('autoplay.stop.owl')
+
+    })
+
+    var $grid = $('.grid').isotope({
+    });
+    $('.filter').on( 'click', 'a', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+        });
+ 
+})
